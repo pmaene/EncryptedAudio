eab3b1b9e5ba14eedb486b82ea404c80aaa497c3
Pieter Maene <pieter@maene.eu>
2013-05-22 22:24:18 +0200

    Started working on the function list

--- source/nassert.c
+++ source/nassert.c
@@ -1,10 +1,10 @@
 #pragma DATA_ALIGN(encryptedData);
 
 void _encryptData(unsigned char *restrict encryptedData, uint8_t *restrict aesKey, uint8_t *restrict nonce, uint32_t packetCounter, unsigned char *restrict dataToEncrypt, size_t dataSize) {
-...
-_nassert(((int) (array) % 8)==0);
-#pragma MUST_ITERATE(aes_BLOCK_SIZE,,8)
-#pragma UNROLL(8)
-for (i = 0; i < aes_BLOCK_SIZE; i++)
-	encryptedData[i+blockCounter*aes_BLOCK_SIZE] = encryptedBlock[i] ^ dataToEncrypt[i+blockCounter*aes_BLOCK_SIZE];
+    ...
+    _nassert(((int) (array) % 8)==0);
+    #pragma MUST_ITERATE(aes_BLOCK_SIZE,,8)
+    #pragma UNROLL(8)
+    for (i = 0; i < aes_BLOCK_SIZE; i++)
+        encryptedData[i+blockCounter*aes_BLOCK_SIZE] = encryptedBlock[i] ^ dataToEncrypt[i+blockCounter*aes_BLOCK_SIZE];
 }
